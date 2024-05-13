30ff670993efa9cc94d4bf41adb16845e214e260
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2024-05-14 04:56:53 +0800

    test: 增加 WS 客户端的测试

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,9 +17,14 @@ async def dg_lab_ws_server_fixture() -> DGLabWSServer:
         yield dg_lab_ws_server
 
 
+@pytest.fixture(name="dg_lab_local_client", scope="session")
+def dg_lab_local_client_fixture(dg_lab_ws_server: DGLabWSServer) -> DGLabLocalClient:
+    return dg_lab_ws_server.new_local_client()
+
+
 @pytest_asyncio.fixture(name="websocket", scope="session")
 async def websocket_fixture() -> WebSocketClientProtocol:
-    async with connect("wss://localhost:5678") as websocket:
+    async with connect("ws://localhost:5678") as websocket:
         yield websocket
 
 
@@ -28,15 +33,27 @@ def dg_lab_ws_client_fixture(websocket: WebSocketClientProtocol) -> DGLabWSClien
     return DGLabWSClient(websocket)
 
 
-@pytest.fixture(name="dg_lab_local_client", scope="session")
-def dg_lab_local_client_fixture(dg_lab_ws_server: DGLabWSServer) -> DGLabLocalClient:
-    return dg_lab_ws_server.new_local_client()
+def test_dg_lab_ws_server(dg_lab_ws_server: DGLabWSServer):
+    assert dg_lab_ws_server.heartbeat_interval == 10
 
 
 def test_dg_lab_local_client(dg_lab_local_client: DGLabLocalClient):
     assert dg_lab_local_client.client_id is not None
     assert dg_lab_local_client.not_registered is False
     assert dg_lab_local_client.not_bind is True
+
+
+@pytest.mark.asyncio
+async def test_dg_lab_ws_client(dg_lab_ws_client: DGLabWSClient):
+    assert dg_lab_ws_client.client_id is None
+    assert dg_lab_ws_client.not_registered is True
+    assert dg_lab_ws_client.not_bind is True
+
+    await dg_lab_ws_client.register()
+
+    assert dg_lab_ws_client.client_id is not None
+    assert dg_lab_ws_client.not_registered is False
+    assert dg_lab_ws_client.not_bind is True
 
 
 @pytest.mark.parametrize(
@@ -62,10 +79,11 @@ def test_dg_lab_local_client(dg_lab_local_client: DGLabLocalClient):
         ),
     ]
 )
-def test_dg_lab_local_client_get_qrcode(
+def test_dg_lab_client_get_qrcode(
         args,
         expected_generator,
-        dg_lab_local_client: DGLabLocalClient
+        dg_lab_local_client: DGLabLocalClient,
+        dg_lab_ws_client: DGLabWSClient
 ):
-    assert dg_lab_local_client.get_qrcode(*args) == \
-           expected_generator(dg_lab_local_client.client_id)
+    for client in dg_lab_local_client, dg_lab_ws_client:
+        assert client.get_qrcode(*args) == expected_generator(client.client_id)
