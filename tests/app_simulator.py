aaeca9026760bd94e65c32b3807239d873a1c85c
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2024-05-14 22:06:21 +0800

    test: 修复 App _recv_owned

--- tests/app_simulator.py
+++ tests/app_simulator.py
@@ -25,9 +25,10 @@ class DGLabAppSimulator:
         return WebSocketMessage.model_validate_json(raw_message)
 
     async def _recv_owned(self) -> WebSocketMessage:
-        message = await self._recv()
-        if message.client_id == self.target_id:
-            return message
+        while True:
+            message = await self._recv()
+            if message.target_id == self.target_id:
+                return message
 
     async def register(self):
         message = await self._recv()
